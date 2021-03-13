eef87725c225dd401f91334186e37a558011c687
kosuke.iwasaki <17j1014g@shisnshu-u.ac.jp>
2021-03-13 13:14:47 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_050142) do
+ActiveRecord::Schema.define(version: 2021_03_13_125828) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
